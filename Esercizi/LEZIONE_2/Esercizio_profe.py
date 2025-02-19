ebd7f429e91686b7dbdfee5e3c265f8e1b755c45
malaonda222 <bandinelli.lisa@gmail.com>
2025-02-19 19:26:03 +0100

    new exercise

---
+++ Esercizi/LEZIONE_2/Esercizio_profe.py
@@ -0,0 +1,12 @@
+'''Data una stringa contenente solo i caratteri '(' e ')', restituire True se le parentesi sono "bilanciate" e False altrimenti.
+Ad esempio: 
+
+bilanciate("((()))") -> True
+
+bilanciate("(()") -> False
+
+bilanciate("()()()") -> True
+
+bilanciate("))((") -> False
+
+bilanciate("(()())") -> True'''
