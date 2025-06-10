28e8f4581cc565778061ba629dec4823d208fe86
Lisa Bandinelli <bandinelli.lisa@gmail.com>
2025-06-10 16:29:59 +0200

    Update Ricerca_Binaria.py
    new changes

--- Esercizi/RECUPERO/Ricerca_Binaria.py
+++ Esercizi/RECUPERO/Ricerca_Binaria.py
@@ -1,14 +1,26 @@
 def ricerca_binaria(lista: list[int], n: int) -> bool:
-    inizio = lista[0]
-    fine = len(lista) - 1
-    medio = (inizio + fine) // 2
+    for item in lista:
+        if type(item) != int:
+            raise ValueError("La lista contiene caratteri diversi da interi.")
+        else:
+            continue 
+    
+    inizio = 0
+    lista_ordinata = sorted(lista)
+    fine = len(lista_ordinata) - 1
+    
     while inizio <= fine:
-        if n == lista[medio]:
+        medio = (inizio + fine) // 2
+        if n == lista_ordinata[medio]:
             return True
-        if n > lista[medio]:
+        if n > lista_ordinata[medio]:
             inizio = medio + 1
         else:
             fine = medio - 1
     return False
         
+input_utente = ricerca_binaria([1, 2, 87, 6, 50, 9, 10, 11, 27, 88, 100, 101], 11)
+print(input_utente)
 
+input2_utente = ricerca_binaria([8, 9, 55, 69, 400, 6785, 8995, -3], 7)
+print(input2_utente)
