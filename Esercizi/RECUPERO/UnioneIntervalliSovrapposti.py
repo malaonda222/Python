b2c1a140692acc77a586bc54d228759e528b4816
malaonda222 <bandinelli.lisa@gmail.com>
2025-06-13 19:27:35 +0200

    new commit

---
+++ Esercizi/RECUPERO/UnioneIntervalliSovrapposti.py
@@ -0,0 +1,6 @@
+#ordinare in ordine crescente rispetto all'indice 0
+
+def merge_intervals(intervals: list[int, int]) -> lista[]
+
+'''se abbiamo una lista di tre intervalli che non si sovrappongono allora:
+se il primo non si sovrappone con il secondo allora il primo non si sovrappone con il terzo'''
