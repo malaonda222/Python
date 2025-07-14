70990e9459781a86f29feef28e21d4c1527cb1b2
malaonda222 <bandinelli.lisa@gmail.com>
2025-07-14 12:05:50 +0200

    commit

---
+++ ESERCIZI CHAT GPT/11.py
@@ -0,0 +1,11 @@
+def word_frequency(text: str) -> dict:
+    my_dict = {}
+    parole = text.split()
+    for parola in parole:
+        if parola not in my_dict:
+            my_dict[parola] = 1 
+        else:
+            my_dict[parola] += 1 
+    return my_dict 
+
+print(word_frequency("ciao mondo ciao"))
