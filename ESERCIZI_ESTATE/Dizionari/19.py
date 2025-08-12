7e329c6f2dcd4d2990d2d03259211c2cbe9fb619
malaonda222 <bandinelli.lisa@gmail.com>
2025-08-12 20:47:01 +0200

    commit

---
+++ ESERCIZI_ESTATE/Dizionari/19.py
@@ -0,0 +1,16 @@
+'''Esercizio: voto_massimo_sotto
+
+Scrivi una funzione chiamata voto_massimo_sotto che, data una lista di studenti (ognuno rappresentato da un dizionario con nome e lista di voti numerici) e una soglia numerica, ritorni un dizionario che associa a ogni studente il voto massimo tra quelli inferiori alla soglia.
+
+Se uno studente non ha voti inferiori alla soglia, il voto massimo sarà None.
+Esempio di input:
+
+studenti = [
+    {"nome": "Marco", "voti": [10, 25, 30, 18]},
+    {"nome": "Giulia", "voti": [28, 16, 12]},
+    {"nome": "Elena", "voti": [24, 26]},
+]
+
+soglia = 20'''
+
+
