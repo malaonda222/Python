60db6a33caf5f679bfcd56fa94c3705649ad5a02
malaonda222 <bandinelli.lisa@gmail.com>
2025-10-23 17:05:38 +0200

    commit

--- Esercizi/LEZIONE_23/FLASK/Eserciziurl_for/esercizio3.py
+++ Esercizi/LEZIONE_23/FLASK/Eserciziurl_for/esercizio3.py
@@ -29,3 +29,28 @@ def posts() -> list:
     risposta += f"<h4>Id48 - {url_id48}</h4>"
 
     return risposta 
+
+
+
+#oppure 
+app = Flask(__name__)
+
+@app.route('/')
+def home() -> str:
+    return "" 
+
+@app.route('/id/<int:id>')
+def mostra_id(id: int) -> str:
+    return f"Id numero: {id}"
+
+@app.route('/ids')
+def ids() -> str:
+    elenco_id = [
+        1, 10, 22, 48
+    ]
+
+    result = "<h3>Elenco degli id</h3>"
+    for id in elenco_id:
+        url = url_for('mostra_id', id = id)
+        result += f"<h4>{id} - <a href='{url}'>{url}</a></h4>"
+    return result 
