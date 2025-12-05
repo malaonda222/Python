f8ef0886838372c0cabbf53b9fd37b17e0d72c93
malaonda222 <bandinelli.lisa@gmail.com>
2025-12-05 12:17:16 +0100

    commit

---
+++ Esercizi/LEZIONE_23/FLASK/test.py
@@ -0,0 +1,37 @@
+import requests 
+import json 
+
+if __name__ == "__main__":
+    headers = {             #serve per descrivere la tipologia del dato utilizzato 
+        'Content-type': 'application/json', #il valore cambia in base al formato 
+        'Accept': 'application/json' #il valore cambia in base al formato
+    }
+
+    # Esempio di GET
+    response = requests.get(
+        url= 'http://localhost:5000/libri',
+        headers=headers
+    )
+    print('Risposta GET:', response.json()) #stampo nel terminale il contenuto del json che corrisponde alla risposta della chiamata get
+    
+    
+    # Esempio di POST
+    payload = {
+        'nome': 'Marco',
+        'cognome': 'Cascio'
+    }
+    # Versione senza dumps
+    response_post = requests.post(
+        url='http://localhost:5000/api/utenti', #esempio di endpoint 
+        json=payload,
+        headers=headers
+    )
+
+    # Versione con dumps
+    response_post = requests.post(
+        url='http://localhost:5000/api/utenti',
+        data=json.dumps(payload),
+        headers=headers
+    )
+    print('Risposta POST:', response_post.json())
+
